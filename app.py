ab6bd42f669140dc3af83ce5bd7349bc4d928675
piyush jain <jainrachita15@yahoo.in>
2020-11-25 23:11:01 -0500

    db connection

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 ﻿from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['PORT'] = '3306'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'bookstoreproject'
+app.config['MYSQL_DB'] = 'project'
 
 mysql = MySQL(app)
 
@@ -17,7 +19,7 @@ def index():
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
 	if request.method == "POST":
-		print(request.form.get('password'))
+		print(request.form['password'])
 		details = request.form
 		print("details")
 		
@@ -34,11 +36,21 @@ def signup():
 
 @app.route('/signin/', methods=['GET', 'POST'])
 def signin():
+	if request.method == "POST":
+		print(request.form['password'])
+		username = request.form['user']
+		password = request.form['password']
+		cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cur.execute("INSERT INTO users(username, passwrd) VALUES (%s, %s)", (username, password))
+		mysql.connection.commit()
+		cur.close()
 	return render_template('signin.html')
 
 @app.route('/books/', methods=['GET', 'POST'])
 def books():
 	return render_template('books.html')
+
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
